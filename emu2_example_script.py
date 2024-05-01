8cf0b12f46ee13d95bf725108a8b0bed7f52ddd5
Gnurro <knawurzelkopp@hotmail.de>
2024-05-01 14:40:36 +0200

    Add example two-GPU code to Emu2 example script

--- emu2_example_script.py
+++ emu2_example_script.py
@@ -2,15 +2,35 @@ from PIL import Image
 import requests
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from accelerate import init_empty_weights, infer_auto_device_map, load_checkpoint_and_dispatch
 
 
 tokenizer = AutoTokenizer.from_pretrained("BAAI/Emu2-Chat")
 
+"""
 model = AutoModelForCausalLM.from_pretrained(
     "BAAI/Emu2-Chat",
     torch_dtype=torch.bfloat16,
     low_cpu_mem_usage=True,
     trust_remote_code=True).to('cuda').eval()
+"""
+
+with init_empty_weights():
+    model = AutoModelForCausalLM.from_pretrained(
+        "BAAI/Emu2-Chat",
+        torch_dtype=torch.bfloat16,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True)
+
+device_map = infer_auto_device_map(model, max_memory={0: '38GiB', 1: '38GiB', },
+                                   no_split_module_classes=['Block', 'LlamaDecoderLayer'])
+# input and output logits should be on same device
+device_map["model.decoder.lm.lm_head"] = 0
+
+model = load_checkpoint_and_dispatch(
+    model,
+    'local/path/to/hf/version/Emu2-Chat/model',
+    device_map=device_map).eval()
 
 # `[<IMG_PLH>]` is the image placeholder which will be replaced by image embeddings.
 # the number of `[<IMG_PLH>]` should be equal to the number of input images
