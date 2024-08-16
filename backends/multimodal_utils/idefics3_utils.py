34724d5815b52c3d56c2b0de4ce26acb09d6470e
kushal-10 <kushalkoshti11@gmail.com>
2024-08-16 09:59:24 +0200

    fix2

--- backends/multimodal_utils/idefics3_utils.py
+++ backends/multimodal_utils/idefics3_utils.py
@@ -110,8 +110,8 @@ class Idefics3MLLM(BaseMLLM):
             processed_images.append(load_image(image))
 
         processed_prompt = handler.apply_chat_template(prompt, add_generation_prompt=True)
-        inputs = handler(text=processed_prompt, images=processed_images, return_tensors="pt").to(device)
-        inputs = {k: v.to(device) for k, v in inputs.items()}
+        inputs = handler(text=processed_prompt, images=processed_images, return_tensors="pt")
+        # inputs = {k: v.to(device) for k, v in inputs.items()}
 
         # Generate
         generated_ids = model.generate(**inputs, max_new_tokens=500)
