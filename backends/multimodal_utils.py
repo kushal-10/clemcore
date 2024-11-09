916374ede8fc20972d69d3a07779c75151d8c570
kushal-10 <kushalkoshti11@gmail.com>
2024-11-09 15:23:55 +0100

    update model registry

--- backends/multimodal_utils.py
+++ backends/multimodal_utils.py
@@ -14,7 +14,7 @@ import requests
 from io import BytesIO
 
 
-##### INTERNVL2 + NVLM TYPE MODELS #####
+##### INTERNVL2 TYPE MODELS #####
 
 IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STD = (0.229, 0.224, 0.225)
