11f7b35b6b20bef9bee13009b387dd41691a7f2a
kushal-10 <kushalkoshti11@gmail.com>
2024-11-09 14:28:18 +0100

    more fixes

--- backends/huggingface_multimodal_api.py
+++ backends/huggingface_multimodal_api.py
@@ -221,11 +221,11 @@ class HuggingfaceMultimodalModel(backends.Model):
         self.context_size = get_context_limit(model_spec)
         self.model_name = model_spec['model_name']
 
-        self.split_prefix = model_spec.get('output_split_prefix', "")
-        self.template = model_spec.get('custom_chat_template', None)
-        self.cull = model_spec.get('eos_to_cull', None)
-        self.supports_multiple_images = model_spec.get('supports_multiple_images', False)
-        self.do_sample = model_spec.get('do_sample', None)
+        self.split_prefix = model_spec.output_split_prefix if hasattr(model_spec, 'output_split_prefix') else ""
+        self.template = model_spec.custom_chat_template if hasattr(model_spec, 'custom_chat_template') else None
+        self.cull = model_spec.eos_to_cull if hasattr(model_spec, 'eos_to_cull') else None
+        self.supports_multiple_images = model_spec.supports_multiple_images if hasattr(model_spec, 'supports_multiple_images') else False
+        self.do_sample = model_spec.do_sample if hasattr(model_spec, 'do_sample') else None
 
     def generate_response(self, messages: List[Dict]) -> Tuple[Any, Any, str]:
         """Generate a response based on the provided messages.
