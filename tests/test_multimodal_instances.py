f941efe0a7224bcc4daca15a3c03266a4b9456ec
kushal-10 <kushalkoshti11@gmail.com>
2024-11-10 13:13:30 +0100

    cleanup test instances

--- tests/test_multimodal_instances.py
+++ tests/test_multimodal_instances.py
@@ -1,12 +1,41 @@
+"""
+Creates test instances for MM games and their text only versions
+"""
+
 import json
 
 # game_name = "textmapworld_description"
 
 game_names = [
-    "referencegame"
+    "referencegame",
+    "textmapworld",
+    "textmapworld_graphreasoning",
+    "textmapworld_questions",
+    "textmapworld_specificroom",
+    "textmapworld_description",
+    "matchit",
+    "matchit_1q",
+    "matchit_5q",
+    "matchit_info",
+    "matchit_ascii",
+    "matchit_ascii_1q",
+    "matchit_ascii_5q",
+    "matchit_ascii_info",
+    "multimodal_referencegame",
+    "mm_mapworld",
+    "mm_mapworld_qa",
+    "mm_mapworld_specificroom",
+    "mm_mapworld_graphs"
 ]
 
-def gen_test_instances(game_name:str):
+def gen_test_instances(game_name: str):
+    """Generates test instances for a given game.
+
+    Args:
+        game_name (str): The name of the game for which to generate test instances.
+
+    This function reads game instance data from JSON files, processes it, and writes the test data to a new JSON file.
+    """
     with open(f'games/{game_name}/in/instances.json', 'r') as f:
         json_data = json.load(f)
 
@@ -29,37 +58,29 @@ def gen_test_instances(game_name:str):
 
     exps = json_data['experiments']
     for e in exps:
-        dict_obj = {
-            "name": "",
-            "game_instances": [
-
-            ]
-        }
 
         insts = e["game_instances"]
-        dict_obj["name"] = e["name"]
+        dict_obj = e         
+        dict_obj["game_instances"] = []
         for i in insts:
-            if i["game_id"] == 0: # mm_ref
-            # if i["game_id"] %10 == 0: # textmap
-                dict_obj["game_instances"].append(i)
+            if "textmapworld"  in game_name and "description" not in game_name:
+                if i["game_id"]%10 == 0: # text_map
+                    dict_obj["game_instances"].append(i)
+            else:
+                if i["game_id"] == 0: # mm_ref
+                    dict_obj["game_instances"].append(i)
+                
 
         test_data["experiments"].append(dict_obj)
 
     if pentomino_data:
         exps = pentomino_data['experiments']
         for e in exps:
-            dict_obj = {
-                "name": "",
-                "game_instances": [
-
-                ]
-            }
-
             insts = e["game_instances"]
-            dict_obj["name"] = e["name"]
+            dict_obj = e         
+            dict_obj["game_instances"] = []
             for i in insts:
-                # if i["game_id"] == 0: # mm_ref
-                if i["game_id"] %10 == 0: # textmap
+                if i["game_id"] == 0:
                     dict_obj["game_instances"].append(i)
 
             test_data["experiments"].append(dict_obj)
