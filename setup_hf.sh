bd10d00013ff6796748c397aa5551dcb2ba8e4f2
Philipp Sadler <philipp.sadler@gmail.com>
2025-04-23 12:24:13 +0200

    cleanup: setup scripts moved to clembench

--- setup_hf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 -m venv venv_hf
-source venv_hf/bin/activate
-pip3 install -r requirements.txt
-pip3 install -r requirements_hf.txt
--- setup_vllm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 -m venv venv_vllm
-source venv_vllm/bin/activate
-pip3 install -r requirements.txt
-pip3 install -r requirements_vllm.txt
