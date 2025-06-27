e417a8da9f2e4cdb839b09036dfb28638c8e1242
Philipp Sadler <philipp.sadler@gmail.com>
2025-06-27 17:00:50 +0200

    add script to establish default room layout

---
+++ clemcore/resources/slurk_api/task_room_layout.js
@@ -0,0 +1,9 @@
+// Hide the sidebar
+$('#sidebar').hide();
+
+// Style and center the content
+$('#content').css({
+    'width': '800px',       // or any fixed width you need
+    'margin': '0 auto',
+    'text-align': 'center' // Optional, depends on what kind of content
+});
