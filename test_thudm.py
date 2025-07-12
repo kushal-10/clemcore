41910e6ae3c8eeb2b5e5f329ed18428a42f6aadf
kushal-10 <kushalkoshti11@gmail.com>
2025-07-12 13:12:19 +0200

    test messages

--- test_thudm.py
+++ test_thudm.py
@@ -2,6 +2,7 @@ from transformers import AutoProcessor, Glm4vForConditionalGeneration
 import torch
 
 MODEL_PATH = "THUDM/GLM-4.1V-9B-Thinking"
+
 messages = [
     {
         "role": "user",
@@ -17,6 +18,46 @@ messages = [
         ],
     }
 ]
+clem_messages = [
+    {"role": "user", "content": "describe this image", "image":["https://upload.wikimedia.org/wikipedia/commons/f/fa/Grayscale_8bits_palette_sample_image.png"]},
+    # {"role": "assistant", "content": "some random response"},
+    # {"role": "user", "content": "some random text 2"},
+    # {"role": "assistant", "content": "some random response 2"},
+    # {"role": "user", "content": "some random text 3", "image":["url2"]},
+]
+
+def get_glm_messages(clem_messages):
+    glm_messages = []
+    for msg in clem_messages:
+        msg_dict = {}
+        if msg["role"] == "user":
+            msg_dict["role"] = "user"
+            msg_dict["content"] = [
+                {
+                  "type": "text",
+                  "content": msg["content"],
+                }
+            ]
+            if "image" in msg:
+                for img in msg["image"]:
+                    msg_dict["content"].append({"type": "image", "content": img})
+        elif msg["role"] == "assistant":
+            msg_dict["role"] = "assistant"
+            msg_dict["content"] = [
+                {
+                    "type": "text",
+                    "content": msg["content"],
+                }
+            ]
+
+        glm_messages.append(msg_dict)
+    return glm_messages
+
+
+
+glm_messages = get_glm_messages(clem_messages)
+print(glm_messages)
+
 processor = AutoProcessor.from_pretrained(MODEL_PATH, use_fast=True)
 model = Glm4vForConditionalGeneration.from_pretrained(
     pretrained_model_name_or_path=MODEL_PATH,
