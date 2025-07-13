8c82c9d0bfbf32a5378f2385d221bd816031ff9a
kushal-10 <kushalkoshti11@gmail.com>
2025-07-13 14:46:26 +0200

    test kimi vl

---
+++ test_kimi.py
@@ -0,0 +1,43 @@
+from PIL import Image
+from transformers import AutoModelForCausalLM, AutoProcessor
+
+def extract_thinking_and_summary(text: str, bot: str = "◁think▷", eot: str = "◁/think▷") -> str:
+    if bot in text and eot not in text:
+        return ""
+    if eot in text:
+        return text[text.index(bot) + len(bot):text.index(eot)].strip(), text[text.index(eot) + len(eot) :].strip()
+    return "", text
+
+OUTPUT_FORMAT = "--------Thinking--------\n{thinking}\n\n--------Summary--------\n{summary}"
+
+url = "https://huggingface.co/spaces/moonshotai/Kimi-VL-A3B-Thinking/resolve/main/images/demo6.jpeg"
+
+model_path = "moonshotai/Kimi-VL-A3B-Thinking-2506"
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    torch_dtype="auto",
+    device_map="auto",
+    trust_remote_code=True,
+)
+processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
+
+image_paths = ["url"]
+images = [Image.open(path) for path in image_paths]
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "image": image_path} for image_path in image_paths
+        ] + [{"type": "text", "text": "What kind of cat is this? Answer with one word."}],
+    },
+]
+text = processor.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt")
+inputs = processor(images=images, text=text, return_tensors="pt", padding=True, truncation=True).to(model.device)
+generated_ids = model.generate(**inputs, max_new_tokens=32768, temperature=0.8)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+]
+response = processor.batch_decode(
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+)[0]
+print(response)
